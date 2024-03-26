a8b77c8141621ab03f9bc9a1195696a3b669d8a1
DuongWuangDat <duongquangdat172004@gmail.com>
2024-03-27 00:04:17 +0700

    prepare menu

---
+++ QuanLiGaraOto/View/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuanLiGaraOto.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Overlay_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            BeginStoryboard((Storyboard)Resources["MenuClose"]);
+        }
+
+        private void AdminWD_Closed(object sender, System.EventArgs e)
+        {
+           // this.Owner.Visibility = Visibility.Visible;
+        }
+    }
+}
