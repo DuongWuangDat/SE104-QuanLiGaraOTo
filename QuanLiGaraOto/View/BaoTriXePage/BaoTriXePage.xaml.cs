7575d855779530e7b9e75bb930afb0a6f7084ae3
DuongWuangDat <duongquangdat172004@gmail.com>
2024-03-27 23:51:57 +0700

    save me

---
+++ QuanLiGaraOto/View/BaoTriXePage/BaoTriXePage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuanLiGaraOto.View.BaoTriXePage
+{
+    /// <summary>
+    /// Interaction logic for BaoTriXePage.xaml
+    /// </summary>
+    public partial class BaoTriXePage : Page
+    {
+        public BaoTriXePage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
