ce4ed9fb7871a4f3190258cd492e26469f90e79b
Nguyen Duong <nduongg04@gmail.com>
2024-03-27 17:19:21 +0700

    Add messagebox

---
+++ QuanLiGaraOto/View/MessageBox/DeleteMessageBox.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+
+namespace QuanLiGaraOto.View.MessageBox
+{
+	/// <summary>
+	/// Interaction logic for DeleteMessage.xaml
+	/// </summary>
+	public partial class DeleteMessageBox : UserControl
+	{
+
+		public DeleteMessageBox(string s = "Bạn có chắc chắn muốn xóa không")
+		{
+			InitializeComponent();
+			TextboxShow.Text = s;
+		}
+
+		//private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		//{
+		//	if (e.LeftButton == MouseButtonState.Pressed)
+		//	{
+		//		DragMove();
+		//	}
+		//}
+
+		private void No_btn_Click(object sender, RoutedEventArgs e)
+		{
+			//this.DialogResult = false;
+			//Window.GetWindow(this).Close();
+		}
+
+		private void Yes_btn_Click(object sender, RoutedEventArgs e)
+		{
+			//this.DialogResult = true;
+			//this.Close();
+		}
+	}
+}
---
+++ QuanLiGaraOto/ViewModel/TraCuuXeVM/TraCuuXeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiGaraOto.ViewModel.TraCuuXeVM
+{
+	public class TraCuuXeViewModel
+	{
+		public string Name { get; set; }
+		public TraCuuXeViewModel() { }
+	}
+}
