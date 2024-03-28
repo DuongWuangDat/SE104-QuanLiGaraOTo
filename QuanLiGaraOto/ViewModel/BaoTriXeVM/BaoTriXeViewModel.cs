ca893dc4fed04dcf8bc82810ecb3affb6032026e
DuongWuangDat <duongquangdat172004@gmail.com>
2024-03-28 20:36:09 +0700

    finish phieu bao tri

--- QuanLiGaraOto/ViewModel/BaoTriXeVM/BaoTriXeViewModel.cs
+++ QuanLiGaraOto/ViewModel/BaoTriXeVM/BaoTriXeViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +18,32 @@ namespace QuanLiGaraOto.ViewModel.BaoTriXeVM
 			set { _receptionCount = value; }
 		}
 
+		private List<String> _brandCarList;
+
+		public List<String> BrandCarList
+		{
+            get { return new List<String> { 
+				"Audi", "BMW", "Chevrolet", "Ford", "Honda", "Hyundai", "Kia", "Lexus", "Mazda", "Mercedes-Benz", "Mitsubishi", "Nissan", "Peugeot", "Subaru", "Suzuki", "Toyota", "Volkswagen", "Volvo"
+			}; }
+            set { _brandCarList = value; }
+        }
+
+		private DateTime selectedDate = DateTime.Now;
+
+		public DateTime SelectedDate
+		{
+			get { return selectedDate; }
+			set { selectedDate = value; OnPropertyChanged(); }
+		}
+
+
+
+
+		public BaoTriXeViewModel()
+		{
+			ReceptionCount = 0;
+           
+        }
 
 	}
 }
