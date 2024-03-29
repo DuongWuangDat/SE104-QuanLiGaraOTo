388fa0b58f40a5e5cc07656a83733dc765993e1c
DuongWuangDat <duongquangdat172004@gmail.com>
2024-03-29 10:23:25 +0700

    manage brand car wd

---
+++ QuanLiGaraOto/View/BaoTriXePage/QuanLiHieuXeWD.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuanLiGaraOto.View.BaoTriXePage
+{
+    /// <summary>
+    /// Interaction logic for QuanLiHieuXeWD.xaml
+    /// </summary>
+    public partial class QuanLiHieuXeWD : Window
+    {
+        public QuanLiHieuXeWD()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
