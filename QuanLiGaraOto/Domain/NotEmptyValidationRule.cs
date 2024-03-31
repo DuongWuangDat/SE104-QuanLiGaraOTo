a820d915e818426607a593a38fbc43c12f6f2565
Nguyen Duong <nduongg04@gmail.com>
2024-03-31 22:26:40 +0700

    Add Phieuthutien, Tracuuxe

---
+++ QuanLiGaraOto/Domain/NotEmptyValidationRule.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace QuanLiGaraOto.Domain
+{
+	public class NotEmptyValidationRule : ValidationRule
+	{
+		public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+		{
+			return string.IsNullOrWhiteSpace((value ?? "").ToString())
+				? new ValidationResult(false, "Field is required.")
+				: ValidationResult.ValidResult;
+		}
+	}
+}
+
+
