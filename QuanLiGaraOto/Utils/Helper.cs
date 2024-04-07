fa921e93cfe2153bfcb6cdeaefecce85750f3bc8
DuongWuangDat <duongquangdat172004@gmail.com>
2024-04-07 15:11:53 +0700

    add check email, phone func

---
+++ QuanLiGaraOto/Utils/Helper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace QuanLiGaraOto.Utils
+{
+    public class Helper
+    {
+        public static bool IsPhoneNumber(string phoneNumber)
+        {
+            string regex = @"^0\d{9-10}$";
+            return Regex.Match(phoneNumber,regex).Success;
+        }
+
+        public static bool IsEmail(string email)
+        {
+            string regex = @"^[\w]+@([\w]+\.)+[\w]{2,}$";
+            return Regex.Match(email, regex).Success;
+        }
+    }
+}
